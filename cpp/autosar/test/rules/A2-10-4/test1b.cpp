213047771ca2a0b4eccefb09d1d85c40bc55411d
Jeroen Ketema <jketema@github.com>
2022-07-19 20:31:42 +0200

    Upgrade `github/codeql` dependency to 2.10.5

--- cpp/autosar/test/rules/A2-10-4/test1b.cpp
+++ cpp/autosar/test/rules/A2-10-4/test1b.cpp
@@ -3,7 +3,6 @@ static int v1 = 3; // NON_COMPLIANT
 } // namespace ns1
 
 namespace ns3 {
-static void f1() {} // NON_COMPLIANT - Not accepted by Clang linker and
-                    // therefore not alerted upon.
+static void f1() {} // NON_COMPLIANT - Not accepted by Clang linker
 void f2() {}        // COMPLIANT - Not accepted by Clang linker
 } // namespace ns3
