f73ee762f7b469cdf7f12253df94fe6056eb5b0d
John L. Singleton <jsinglet@github.com>
2022-08-08 14:11:59 -0400

    fix up test case

--- cpp/cert/test/rules/CON50-CPP/test.cpp
+++ cpp/cert/test/rules/CON50-CPP/test.cpp
@@ -7,19 +7,19 @@ void t3(int i, std::mutex *pm) { delete pm; }
 
 void f1() {
   std::thread threads[5];
-  std::mutex m1;
+  std::mutex m1; // NON_COMPLIANT
 
   for (int i = 0; i < 5; ++i) {
-    threads[i] = std::thread(t1, i, &m1); // NON_COMPLIANT
+    threads[i] = std::thread(t1, i, &m1);
   }
 }
 
 void f2() {
   std::thread threads[5];
-  std::mutex m1;
+  std::mutex m1; // COMPLIANT - due to check below
 
   for (int i = 0; i < 5; ++i) {
-    threads[i] = std::thread(t1, i, &m1); // COMPLIANT - due to check below
+    threads[i] = std::thread(t1, i, &m1);
   }
 
   for (int i = 0; i < 5; ++i) {
@@ -27,26 +27,25 @@ void f2() {
   }
 }
 
-std::mutex m2;
+std::mutex m2; // COMPLIANT - since m2 will not go out of scope.
 
 void f3() {
   std::thread threads[5];
 
   for (int i = 0; i < 5; ++i) {
-    threads[i] = std::thread(
-        t1, i, &m2); // COMPLIANT - since m2 will not go out of scope.
+    threads[i] = std::thread(t1, i, &m2);
   }
 }
 
 std::mutex *m3;
 
 void f4() {
-  m3 = new std::mutex();
+  m3 = new std::mutex(); // COMPLIANT
 
   std::thread threads[5];
 
   for (int i = 0; i < 5; ++i) {
-    threads[i] = std::thread(t1, i, m3); // COMPLIANT
+    threads[i] = std::thread(t1, i, m3);
   }
 
   // since we wait here, and the local function created the
@@ -59,22 +58,22 @@ void f4() {
 }
 
 void f5() {
-  m3 = new std::mutex();
+  m3 = new std::mutex(); // COMPLIANT
 
   std::thread threads[5];
 
   for (int i = 0; i < 5; ++i) {
-    threads[i] = std::thread(t2, i, &m3); // COMPLIANT
+    threads[i] = std::thread(t2, i, &m3);
   }
 }
 
 void f6() {
-  m3 = new std::mutex();
+  m3 = new std::mutex(); // COMPLIANT
 
   std::thread threads[5];
 
   for (int i = 0; i < 5; ++i) {
-    threads[i] = std::thread(t1, i, m3); // COMPLIANT
+    threads[i] = std::thread(t1, i, m3);
   }
 
   for (int i = 0; i < 5; ++i) {
@@ -85,13 +84,12 @@ void f6() {
 }
 
 void f7() {
-  m3 = new std::mutex();
+  m3 = new std::mutex(); // NON_COMPLIANT - t3 will attempt to delete the mutex.
 
   std::thread threads[5];
 
   for (int i = 0; i < 5; ++i) {
-    threads[i] = std::thread(
-        t3, i, m3); // NON_COMPLIANT - t3 will attempt to delete the mutex.
+    threads[i] = std::thread(t3, i, m3);
   }
 
   for (int i = 0; i < 5; ++i) {
@@ -100,12 +98,12 @@ void f7() {
 }
 
 void f8() {
-  std::mutex *m = new std::mutex();
-  delete m; // COMPLIANT
+  std::mutex *m = new std::mutex(); // COMPLIANT
+  delete m;
 }
 
 void f9() {
   std::mutex m; // COMPLIANT
 }
 
-std::mutex *m4 = new std::mutex();
+std::mutex *m4 = new std::mutex(); // COMPLIANT
