c54c1ccebeda6ae14d97f0d66ef3502696736ce2
Luke Cartey <lcartey@github.com>
2022-08-18 10:30:39 +0100

    A2-10-5: Failing test for variable templates
    Variable templates generate multiple Variable's for each instantiation
    and the uninstantiated template, causing false positives.

--- cpp/autosar/test/rules/A2-10-5/test1b.cpp
+++ cpp/autosar/test/rules/A2-10-5/test1b.cpp
@@ -8,3 +8,13 @@ static int g3 = 0; // NON_COMPLIANT
 }
 
 static void f1() {} // NON_COMPLIANT
+
+template<class T> constexpr T number_two = T(1); // NON_COMPLIANT
+
+int test3() {
+  return number_two<int>;
+}
+
+long test4() {
+  return number_two<long>;
+}
