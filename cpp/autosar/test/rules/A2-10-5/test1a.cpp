c6d26d3ea745f63c0773056bffa727113aa778ec
Luke Cartey <lcartey@github.com>
2022-08-18 10:55:29 +0100

    A2-10-5: Format test files

--- cpp/autosar/test/rules/A2-10-5/test1a.cpp
+++ cpp/autosar/test/rules/A2-10-5/test1a.cpp
@@ -6,24 +6,16 @@ static int g2;      // COMPLIANT
 static int g3 = 1;  // NON_COMPLIANT
 static void f1(){}; // NON_COMPLIANT
 
-// Variable template has multiple declarations: one for the uninstantiated template and one for each
-// instantiation
-template<class T> constexpr T number_one = T(1); // COMPLIANT
+// Variable template has multiple declarations: one for the uninstantiated
+// template and one for each instantiation
+template <class T> constexpr T number_one = T(1); // COMPLIANT
 
-int test() {
-  return number_one<int>;
-}
+int test() { return number_one<int>; }
 
-long test2() {
-  return number_one<long>;
-}
+long test2() { return number_one<long>; }
 
-template<class T> constexpr T number_two = T(1); // NON_COMPLIANT
+template <class T> constexpr T number_two = T(1); // NON_COMPLIANT
 
-int test3() {
-  return number_two<int>;
-}
+int test3() { return number_two<int>; }
 
-long test4() {
-  return number_two<long>;
-}
+long test4() { return number_two<long>; }
