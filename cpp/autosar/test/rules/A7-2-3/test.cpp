dd889c4fbc90fe37c9c665c7877dbb206efd8043
Mauro Baluda <mbaluda@github.com>
2022-11-03 17:24:37 +0100

    Reformat test files with clang-format v14

--- cpp/autosar/test/rules/A7-2-3/test.cpp
+++ cpp/autosar/test/rules/A7-2-3/test.cpp
@@ -1,12 +1,24 @@
 enum C // NON_COMPLIANT
-{ c1 };
+{
+  c1
+};
 enum class A // COMPLIANT - Violates a different rule (A7-2-2)
-{ a1 };
+{
+  a1
+};
 enum struct A1 // COMPLIANT - Violates a different rule (A7-2-2)
-{ a12 };
+{
+  a12
+};
 enum class B : int // COMPLIANT
-{ b1 };
+{
+  b1
+};
 enum struct B1 : int // COMPLIANT
-{ b12 };
+{
+  b12
+};
 enum D : int // NON_COMPLIANT
-{ d1 };
+{
+  d1
+};
