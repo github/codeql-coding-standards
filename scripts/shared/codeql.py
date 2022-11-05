bc46b7544cdab63ded9cb83c8af7e9155741e03f
Remco Vermeulen <rvermeulen@users.noreply.github.com>
2022-11-04 17:48:16 -0700

    Address incorrect type annotations

--- scripts/shared/codeql.py
+++ scripts/shared/codeql.py
@@ -20,7 +20,7 @@ class CodeQLError(Exception):
 
 
 class CodeQL():
-    def __init__(self) -> 'CodeQL':
+    def __init__(self) -> None:
         codeql_result = subprocess.run(
             ["codeql", "version", "--format=json"], capture_output=True)
         if not codeql_result.returncode == 0:
@@ -36,7 +36,7 @@ class CodeQL():
             raise CodeQLError(
                 f"Failed to retrieve codeql version information with error: {e.msg}")
 
-    def __build_command_options(self, **options: Dict[str, str]) -> List[str]:
+    def __build_command_options(self, **options: str) -> List[str]:
         command_options = []
         for key, value in options.items():
             command_options.append(f"--{key.replace('_', '-')}")
@@ -59,7 +59,7 @@ class CodeQL():
             raise CodeQLError(
                 f"Unable to cleanup database {database_path}", stdout=result.stdout, stderr=result.stderr, returncode=result.returncode)
 
-    def run_queries(self, database_path: Path, *queries: List[Path], **options: Dict[str, str]) -> None:
+    def run_queries(self, database_path: Path, *queries: Path, **options: str) -> None:
         database_path = database_path.resolve()
 
         command_options = self.__build_command_options(**options)
@@ -91,7 +91,7 @@ class CodeQL():
         with qlpack_path.open() as f:
             return yaml.safe_load(f)
 
-    def decode_results(self, database_path: Path, query_path: Path, **options: Dict[str, str]) -> Iterator:
+    def decode_results(self, database_path: Path, query_path: Path, **options: str) -> List:
         qlpack_path = self.resolve_qlpack_path(query_path)
         qlpack = self.get_qlpack(qlpack_path)
         relative_query_path = query_path.relative_to(qlpack_path.parent)
@@ -118,7 +118,7 @@ class CodeQL():
             with open(temp_file) as tmp:
                 return csv.reader(tmp)
 
-    def generate_query_help(self, query_help_path: Path, output: Path, format : str = "markdown", **options: Dict[str, str]) -> None:
+    def generate_query_help(self, query_help_path: Path, output: Path, format : str = "markdown", **options: str) -> None:
         command = ['codeql', 'generate', 'query-help']
         options['output'] = str(output)
         options['format'] = format
