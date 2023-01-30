47805d8a2ff70025fbdf8cbdabc8a22f0ef5a944
Jeongsoo Lee <jeongsoolee09@github.com>
2023-01-30 15:40:54 -0800

    update

--- cpp/common/test/rules/unusedparameter/test.cpp
+++ cpp/common/test/rules/unusedparameter/test.cpp
@@ -1,6 +1,8 @@
 // NOTICE: SOME OF THE TEST CASES BELOW ARE ALSO INCLUDED IN THE C TEST CASE AND
 // CHANGES SHOULD BE REFLECTED THERE AS WELL.
 
+#include <tuple>
+
 int test_used(int x) { return x; } // COMPLIANT
 
 void test_unused(int x) {} // NON_COMPLIANT
@@ -15,5 +17,17 @@ class A {
   void c(int) {}                  // COMPLIANT
   virtual void d(int x, int y) {} // virtual, not covered by this rule
 };
+
+ void f(
+   int i,
+   int j,
+   int k,
+   [[maybe_unused]]
+   int l                   // NON_COMPILANT: maybe_unused parameters should also be considered unused
+ ) {
+    static_cast<void>(i);  // NON_COMPILANT: static_cast to void should also be considered unused
+    (void)j;               // NON_COMPILANT: C-style void casts should also be considered unused
+    std::ignore = k;       // NON_COMPILANT: Assignment to std::ignore should also be considered unused
+ }
 
 void test_no_def(int x); // COMPLIANT - no definition, so cannot be "unused"
