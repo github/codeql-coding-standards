e49bcd6aae346173a4097beea3dee09d1a2716e1
Jeongsoo Lee <jeongsoolee09@github.com>
2023-01-31 08:46:13 -0800

    update test.c for RULE-6-1 to differ from given examples

--- c/misra/test/rules/RULE-6-1/test.c
+++ c/misra/test/rules/RULE-6-1/test.c
@@ -1,15 +1,13 @@
-typedef unsigned int UINT_16;
+typedef unsigned int UINT16;
 
 enum Color { R, G, B };
 
 struct SampleStruct {
-  unsigned int b1 : 2; // COMPILANT - explicitly unsigned (example in the doc)
-  signed int b2 : 2;   // COMPILANT - explicitly signed
-  int b3 : 2; // NON_COMPLIANT - plain int not permitted (example in the doc)
-  UINT_16 b4 : 2; // COMPLIANT - typedef designating unsigned int (example in
-                  // the doc)
-  signed long b5 : 2; // NON_COMPLIANT - even if long and int are the same size
-                      // (example in the doc)
-  signed char b6 : 2; // NON_COMPILANT - cannot declare bit field for char
-  enum Color b7 : 3;  // NON_COMPILANT - cannot declare bit field for enum
+  int x1 : 2; // NON_COMPLIANT - not explicitly signed or unsigned
+  unsigned int x2 : 2; // COMPILANT - explicitly unsigned (example in the doc)
+  signed int x3 : 2;   // COMPILANT - explicitly signed
+  UINT16 x4 : 2; // COMPLIANT - type alias resolves to a compliant type
+  signed long x5 : 2; // NON_COMPLIANT - cannot declare bit field for long, even if it's signed
+  signed char x6 : 2; // NON_COMPILANT - cannot declare bit field for char, even if it's signed
+  enum Color x7 : 3;  // NON_COMPILANT - cannot declare bit field for enum
 } sample_struct;
