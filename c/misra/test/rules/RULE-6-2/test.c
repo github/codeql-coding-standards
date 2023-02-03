b8d2b95e8f1c7fc429b304b64e7d0796624f31e1
Jeongsoo Lee <jeongsoolee09@github.com>
2023-02-02 18:09:20 -0800

    s/COMPILANT/COMPLIANT/

--- c/misra/test/rules/RULE-6-2/test.c
+++ c/misra/test/rules/RULE-6-2/test.c
@@ -1,17 +1,17 @@
 #include <stdint.h>
 
 struct SampleStruct {
-  int x1 : 1; // NON_COMPILANT: very likely be signed, but if it's not, the
+  int x1 : 1; // NON_COMPLIANT: very likely be signed, but if it's not, the
               // query will automatically handle it since we use signed(), not
               // isExplicitlySigned().
-  signed int x2 : 1; // NON_COMPILANT: single-bit named field with a signed type
+  signed int x2 : 1; // NON_COMPLIANT: single-bit named field with a signed type
   signed char
-      x3 : 1; // NON_COMPILANT: single-bit named field with a signed type
+      x3 : 1; // NON_COMPLIANT: single-bit named field with a signed type
   signed short
-      x4 : 1; // NON_COMPILANT: single-bit named field with a signed type
+      x4 : 1; // NON_COMPLIANT: single-bit named field with a signed type
   unsigned int
-      x5 : 1; // COMPILANT: single-bit named field but with an unsigned type
-  signed int x6 : 2; // COMPILANT: named field with a signed type but declared
+      x5 : 1; // COMPLIANT: single-bit named field but with an unsigned type
+  signed int x6 : 2; // COMPLIANT: named field with a signed type but declared
                      // to carry more than 1 bit
-  signed char : 1;   // COMPILANT: single-bit bit-field but unnamed
+  signed char : 1;   // COMPLIANT: single-bit bit-field but unnamed
 } sample_struct;
