d3d4bd368ecb31cebd82f32d64e802869dcfe58a
Mauro Baluda <mbaluda@github.com>
2023-02-08 17:31:07 +0100

    Revert "Replace .codeqlmanifest.json with codeql-workspace.yml"
    This reverts commit f515ab5bc19a7625492bd07ceaf88ca8df988cf0.

--- scripts/get_workspace_packs.py
+++ scripts/get_workspace_packs.py
@@ -1,14 +1,14 @@
 import glob
-import yaml
+import json
 import os
 
 def get_workspace_packs(root):
-    # Find the packs by globbing using the 'provide' patterns in the CodeQL workspace file.
+    # Find the packs by globbing using the 'provide' patterns in the manifest.
     os.chdir(root)
-    with open('codeql-workspace.yml') as codeql_workspace_file:
-        codeql_workspace = yaml.load(codeql_workspace_file)
+    with open('.codeqlmanifest.json') as manifest_file:
+        manifest = json.load(manifest_file)
     packs = []
-    for pattern in codeql_workspace['provide']:
+    for pattern in manifest['provide']:
         packs.extend(glob.glob(pattern, recursive=True))
-    
+
     return packs
