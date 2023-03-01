dd39c9437e654d5b978111bfde081ac0d3d5afb6
Jeroen Ketema <jketema@github.com>
2023-03-01 10:50:35 +0100

    Update MISRA RULE-8-4 test for changes in latest CodeQL

--- c/misra/test/rules/RULE-8-4/object1.c
+++ c/misra/test/rules/RULE-8-4/object1.c
@@ -5,6 +5,6 @@ extern int i1 = 0; // NON_COMPLIANT
 
 int i2 = 0; // NON_COMPLIANT
 
-extern int i3; // NON_COMPLIANT
+extern int i3; // NON_COMPLIANT - not detected as `short i3` exists 
 
 extern int i4; // COMPLIANT
--- c/misra/test/rules/RULE-8-4/object2.c
+++ c/misra/test/rules/RULE-8-4/object2.c
@@ -1,3 +1,3 @@
-short i3 = 0; // NON_COMPLIANT
+short i3 = 0; // NON_COMPLIANT - not detected as `extern int i3` exists
 
 signed int i4 = 0; // COMPLIANT
