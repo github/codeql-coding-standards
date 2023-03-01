87a0a3df9407d2820a245794e5b808b9317a6886
Jeongsoo Lee <jeongsoolee09@github.com>
2023-02-28 16:29:55 -0800

    Draft of 4-6

--- c/misra/src/rules/DIR-4-6/PlainNumericalTypeUsedOverExplicitTypedef.ql
+++ c/misra/src/rules/DIR-4-6/PlainNumericalTypeUsedOverExplicitTypedef.ql
@@ -13,7 +13,40 @@
 import cpp
 import codingstandards.c.misra
 
-from
-where
-  not isExcluded(x, TypesPackage::plainNumericalTypeUsedOverExplicitTypedefQuery()) and
-select
+abstract class ForbiddenType extends Type { }
+
+class BuiltinNumericType extends ForbiddenType {
+  BuiltinNumericType() {
+    /* Exclude the plain char because it does not count as a numeric type */
+    this.(CharType).isExplicitlySigned()
+    or
+    this.(CharType).isExplicitlyUnsigned()
+    or
+    this instanceof ShortType
+    or
+    this instanceof IntType
+    or
+    this instanceof LongType
+    or
+    this instanceof LongLongType
+    or
+    this instanceof FloatType
+    or
+    this instanceof DoubleType
+    or
+    this instanceof LongDoubleType
+  }
+}
+
+class ForbiddenTypedefType extends ForbiddenType, TypedefType {
+  ForbiddenTypedefType() {
+    this.(TypedefType).getBaseType() instanceof BuiltinNumericType and
+    not this.getName().regexpMatch("u?(int|float)(4|8|16|32|64|128)_t")
+  }
+}
+
+/* TODO: BuiltinNumericType not being flagged */
+from ForbiddenType forbiddenType
+where not isExcluded(forbiddenType, TypesPackage::plainNumericalTypeUsedOverExplicitTypedefQuery())
+select forbiddenType,
+  "The type " + forbiddenType + " is not a fixed-width numeric type nor an alias to one."
--- c/misra/test/rules/DIR-4-6/test.c
+++ c/misra/test/rules/DIR-4-6/test.c
@@ -1,71 +1,66 @@
-#include <stdint.h>
+typedef signed char int8_t;    // COMPLIANT: exception, typedefs are permitted
+typedef unsigned char uint8_t; // COMPLIANT: exception, typedefs are permitted
 
-typedef signed char INT8;    // COMPLIANT: exception, typedefs are permitted
-typedef unsigned char UINT8; // COMPLIANT: exception, typedefs are permitted
+typedef signed short int16_t;    // COMPLIANT: exception, typedefs are permitted
+typedef unsigned short uint16_t; // COMPLIANT: exception, typedefs are permitted
 
-typedef short _INT16;          // COMPLIANT: exception, typedefs are permitted
-typedef signed short INT16;    // COMPLIANT: exception, typedefs are permitted
-typedef unsigned short UINT16; // COMPLIANT: exception, typedefs are permitted
+typedef signed int int32_t;    // COMPLIANT: exception, typedefs are permitted
+typedef unsigned int uint32_t; // COMPLIANT: exception, typedefs are permitted
 
-typedef int _INT32;          // COMPLIANT: exception, typedefs are permitted
-typedef signed int INT32;    // COMPLIANT: exception, typedefs are permitted
-typedef unsigned int UINT32; // COMPLIANT: exception, typedefs are permitted
+typedef signed long int64_t;    // COMPLIANT: exception, typedefs are permitted
+typedef unsigned long uint64_t; // COMPLIANT: exception, typedefs are permitted
 
-typedef long _INT64;          // COMPLIANT: exception, typedefs are permitted
-typedef signed long INT64;    // COMPLIANT: exception, typedefs are permitted
-typedef unsigned long UINT64; // COMPLIANT: exception, typedefs are permitted
-
-typedef long long _INT128;       // COMPLIANT: exception, typedefs are permitted
-typedef signed long long INT128; // COMPLIANT: exception, typedefs are permitted
+typedef signed long long
+    int128_t; // COMPLIANT: exception, typedefs are permitted
 typedef unsigned long long
-    UINT128; // COMPLIANT: exception, typedefs are permitted
+    uint128_t; // COMPLIANT: exception, typedefs are permitted
 
-typedef float FLOAT32;        // COMPLIANT: exception, typedefs are permitted
-typedef double FLOAT64;       // COMPLIANT: exception, typedefs are permitted
-typedef long double FLOAT128; // COMPLIANT: exception, typedefs are permitted
+typedef float float32_t;        // COMPLIANT: exception, typedefs are permitted
+typedef double float64_t;       // COMPLIANT: exception, typedefs are permitted
+typedef long double float128_t; // COMPLIANT: exception, typedefs are permitted
 
 typedef int8_t
     astronomical_number_t; // COMPLIANT: aliasing a fixed-width numeric typedef
 typedef uint8_t u_astronomical_number_t; // COMPLIANT: aliasing a fixed-width
                                          // numeric typedef
 typedef int
-    astronomical_number_t; // NON_COMPLIANT: aliasing a basic numeric type
+    _astronomical_number_t; // NON_COMPLIANT: aliasing a basic numeric type
 
 int            // COMPLIANT: exception, main's return type can be plain int
 main(int argc, // COMPLIANT: exception, argc's type can be plain int
      char *argv[]) { // COMPLIANT: char is not a basic numeric type
 
   char c1 = 1;          // COMPLIANT: char is not a basic numeric type
-  signed char c2 = 1;   // NON_COMPLIANT: use typedef int8_t  in stdint
-  unsigned char c3 = 1; // NON_COMPLIANT: use typedef uint8_t in stdint
-  INT8 c4 = 1;          // COMPLIANT: typedef used instead
+  signed char c2 = 1;   // NON_COMPLIANT: use typedef int8_t
+  unsigned char c3 = 1; // NON_COMPLIANT: use typedef uint8_t
+  int8_t c4 = 1;        // COMPLIANT: typedef used instead
 
   short s1 = 1;          // NON_COMPLIANT: short is a basic numeric type
-  signed short s2 = 1;   // NON_COMPLIANT: use typedef int16_t  in stdint
-  unsigned short s3 = 1; // NON_COMPLIANT: use typedef uint16_t in stdint
-  INT16 s4 = 1;          // COMPLIANT: typedef used instead
+  signed short s2 = 1;   // NON_COMPLIANT: use typedef int16_t
+  unsigned short s3 = 1; // NON_COMPLIANT: use typedef uint16_t
+  int16_t s4 = 1;        // COMPLIANT: typedef used instead
 
   int i1 = 1;          // NON_COMPLIANT: int is a basic numeric type
-  signed int i2 = 1;   // NON_COMPLIANT: use typedef int32_t  in stdint
-  unsigned int i3 = 1; // NON_COMPLIANT: use typedef uint32_t in stdint
-  INT32 s4 = 1;        // COMPLIANT: typedef used instead
+  signed int i2 = 1;   // NON_COMPLIANT: use typedef int32_t
+  unsigned int i3 = 1; // NON_COMPLIANT: use typedef uint32_t
+  int32_t i4 = 1;      // COMPLIANT: typedef used instead
 
   long l1 = 1;          // NON_COMPLIANT: int is a basic numeric type
-  signed long l2 = 1;   // NON_COMPLIANT: use typedef int64_t  in stdint
-  unsigned long l3 = 1; // NON_COMPLIANT: use typedef uint64_t in stdint
-  INT64 s4 = 1;         // COMPLIANT: typedef used instead
+  signed long l2 = 1;   // NON_COMPLIANT: use typedef int64_t
+  unsigned long l3 = 1; // NON_COMPLIANT: use typedef uint64_t
+  int64_t l4 = 1;       // COMPLIANT: typedef used instead
 
-  long long l1 = 1;          // NON_COMPLIANT: int is a basic numeric type
-  signed long long l2 = 1;   // NON_COMPLIANT: use typedef int128_t  in stdint
-  unsigned long long l3 = 1; // NON_COMPLIANT: use typedef uint128_t in stdint
-  INT128 s4 = 1;             // COMPLIANT: typedef used instead
+  long long ll1 = 1;          // NON_COMPLIANT: int is a basic numeric type
+  signed long long ll2 = 1;   // NON_COMPLIANT: use typedef int128_t
+  unsigned long long ll3 = 1; // NON_COMPLIANT: use typedef uint128_t
+  int128_t ll4 = 1;           // COMPLIANT: typedef used instead
 
-  float f1 = 1;   // NON_COMPLIANT: float is a basic numeric type, use a typedef
-  FLOAT32 f2 = 1; // COMPLIANT: typedef used instead
+  float f1 = 1; // NON_COMPLIANT: float is a basic numeric type, use a typedef
+  float32_t f2 = 1; // COMPLIANT: typedef used instead
 
-  double d1 = 1;  // NON_COMPLIANT: int is a basic numeric type
-  FLOAT64 d2 = 1; // COMPLIANT: typedef used instead
+  double d1 = 1;    // NON_COMPLIANT: int is a basic numeric type
+  float64_t d2 = 1; // COMPLIANT: typedef used instead
 
   long double ld1 = 1; // NON_COMPLIANT: int is a basic numeric type
-  FLOAT128 ld2 = 1;    // COMPLIANT: typedef used instead
+  float128_t ld2 = 1;  // COMPLIANT: typedef used instead
 }
