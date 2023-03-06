c43701f9d83f71553b6ab5cac3f4fe954544b9ad
Mauro Baluda <mbaluda@github.com>
2023-03-06 19:52:24 +0100

    Update object2.c

--- c/misra/test/rules/RULE-8-4/object2.c
+++ c/misra/test/rules/RULE-8-4/object2.c
@@ -1,3 +1,4 @@
-short i3 = 0; // NON_COMPLIANT - not detected as `extern int i3` exists
+// not detected as `extern int i3` exists
+short i3 = 0; // NON_COMPLIANT[FALSE_NEGATIVE]
 
 signed int i4 = 0; // COMPLIANT
