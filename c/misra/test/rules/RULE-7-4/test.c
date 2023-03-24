7de306dd1854b0fbd453d028e1c3611cbf9344fd
Jeongsoo Lee <jeongsoolee09@github.com>
2023-03-23 18:04:30 -0700

    Format test suites

--- c/misra/test/rules/RULE-7-4/test.c
+++ c/misra/test/rules/RULE-7-4/test.c
@@ -1,5 +1,5 @@
-#include <wchar.h>
 #include <stdio.h>
+#include <wchar.h>
 
 void sample1() {
   /* Test for plain char type */
@@ -16,15 +16,15 @@ void sample1() {
 
   /* Test for wide char type */
   const wchar_t *ws1 = L"wide string1"; // COMPLIANT: string literal assigned to
-                                       // a const char* variable
+                                        // a const char* variable
   const register volatile wchar_t *ws2 =
       L"wide string2"; // COMPLIANT: string literal assigned to a const char*
-                      // variable, don't care about the qualifiers
-  wchar_t *ws3 = L"wide string3"; // NON_COMPLIANT: char* variable declared to hold
-                              // a string literal
-  ws3 = L"wide string4";       // NON_COMPLIANT: char* variable assigned a string
-                        // literal (not likely to be seen in production, since
-                        // there is strcpy)
+                       // variable, don't care about the qualifiers
+  wchar_t *ws3 = L"wide string3"; // NON_COMPLIANT: char* variable declared to
+                                  // hold a string literal
+  ws3 = L"wide string4"; // NON_COMPLIANT: char* variable assigned a string
+                         // literal (not likely to be seen in production, since
+                         // there is strcpy)
 }
 
 /* Testing returning a plain string literal */
@@ -39,8 +39,8 @@ const char *sample2(int x) {
 /* Testing returning a wide string literal */
 const wchar_t *w_sample2(int x) {
   if (x == 1)
-    return L"string5"; // COMPLIANT: can return a string literal with return type
-                      // being const char* being const char*
+    return L"string5"; // COMPLIANT: can return a string literal with return
+                       // type being const char* being const char*
   else
     return NULL;
 }
@@ -56,7 +56,7 @@ char *sample3(int x) {
 wchar_t *w_sample3(int x) {
   if (x == 1)
     return L"string6"; // NON_COMPLIANT: can return a string literal with return
-                      // type being char*
+                       // type being char*
   else
     return NULL;
 }
