8eb193a868902db57964fbbd2e220f131ae87aed
Luke Cartey <lcartey@github.com>
2023-08-17 00:23:30 +0100

    Update generate_modules qlpack, and update script.

--- scripts/update_codeql_dependency.sh
+++ scripts/update_codeql_dependency.sh
@@ -10,9 +10,9 @@ fi
 echo "Updating CodeQL dependency $1 to $2."
 
 # update the qlpacks
-find . -name 'qlpack.yml' | grep -v './codeql_modules' | grep -v './scripts' | xargs sed -i -r "s#${1}: [^\s]+#${1}: ${2}#"
+find . -name 'qlpack.yml' | grep -v './codeql_modules' | xargs sed -i -r "s#${1}: [^\s]+#${1}: ${2}#"
 
 # update the lock files
-find . -name 'codeql-pack.lock.yml' | grep -v './codeql_modules' | grep -v './scripts' | xargs sed -i -r -z "s#${1}:\n(\s*)version: [^\s]+\n#${1}:\n\1version: ${2}\n#"
+find . -name 'codeql-pack.lock.yml' | grep -v './codeql_modules' | xargs sed -i -r -z "s#${1}:\n(\s*)version: [^\s]+\n#${1}:\n\1version: ${2}\n#"
 
 echo "Done."
