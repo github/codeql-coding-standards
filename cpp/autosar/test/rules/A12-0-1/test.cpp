01d32f0cbf4c2bfe16d5e4a40fdaa7bbcb68638a
Remco Vermeulen <rvermeulen@users.noreply.github.com>
2024-01-18 18:29:19 -0800

    Fix test file formatting

--- cpp/autosar/test/rules/A12-0-1/test.cpp
+++ cpp/autosar/test/rules/A12-0-1/test.cpp
@@ -69,15 +69,14 @@ class C14 {
 };
 
 class C15 {
-  C15& operator=(C15 const &); // NON_COMPLIANT
+  C15 &operator=(C15 const &); // NON_COMPLIANT
 };
 
-template<typename T>
-class C16 { // COMPLIANT
-  C16() = default;};
+template <typename T> class C16 { // COMPLIANT
+  C16() = default;
+};
 
-template<typename T>
-class C17 { // COMPLIANT
+template <typename T> class C17 { // COMPLIANT
   C17() = default;
   C17(C17 const &) = default;
   C17(C17 &&) = default;
@@ -86,8 +85,7 @@ class C17 { // COMPLIANT
   C17 &operator=(C17 &&) = default;
 };
 
-template<typename T>
-class C18 { // COMPLIANT
+template <typename T> class C18 { // COMPLIANT
   C18() = default;
   C18(C18 const &) = delete;
   C18(C18 &&) = delete;
@@ -96,15 +94,15 @@ class C18 { // COMPLIANT
   C18 &operator=(C18 &&) = delete;
 };
 
-template<typename T>
-class C19 { // COMPLIANT
-  public:
+template <typename T> class C19 { // COMPLIANT
+public:
   explicit C19(T i) : i(i) {}
   C19(C19 const &) = delete;
   C19(C19 &&) = delete;
   virtual ~C19() = default;
   C19 &operator=(C19 const &) = delete;
   C19 &operator=(C19 &&) = delete;
-  private:
-    T i;
+
+private:
+  T i;
 };
