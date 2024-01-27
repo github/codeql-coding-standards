b628c0ce959185a5e85e06475a77ae44f9ac4907
Remco Vermeulen <rvermeulen@users.noreply.github.com>
2024-01-26 18:08:19 -0800

    Pin Nix packages

--- tooling/codeql/default.nix
+++ tooling/codeql/default.nix
@@ -1,5 +1,6 @@
 let
-    pkgs = import <nixpkgs> {};
+    nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/0e148322b344eab7c8d52f6e59b0d95ba73fb62e.tar.gz";
+    pkgs = (import nixpkgs { config = {}; overlays = []; });
 in
 rec {
     ql-extractor-0_0_1 = pkgs.callPackage ./ql-extractor/0.0.1.nix {};
