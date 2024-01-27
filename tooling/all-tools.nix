39126fb7c15f5a3893705e47db58894ee5169246
Remco Vermeulen <rvermeulen@users.noreply.github.com>
2024-01-26 17:21:28 -0800

    Move CodeQL tooling and shell derivative into tooling directory

---
+++ tooling/all-tools.nix
@@ -0,0 +1,1 @@
+import codeql/default.nix
--- tooling/shell.nix
+++ tooling/shell.nix
@@ -1,6 +1,6 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/0e148322b344eab7c8d52f6e59b0d95ba73fb62e.tar.gz";
-  pkgs = (import nixpkgs { config = {}; overlays = []; }) // (import ./scripts/codeql/default.nix);
+  pkgs = (import nixpkgs { config = {}; overlays = []; }) // (import ./all-tools.nix);
 in
 
 pkgs.mkShell {
