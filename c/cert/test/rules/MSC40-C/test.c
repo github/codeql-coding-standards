337604e56719a36e6098e8b8b41114168e3ab39a
Luke Cartey <lcartey@github.com>
2024-01-30 13:14:37 +0000

    Fix test formatting

--- c/cert/test/rules/MSC40-C/test.c
+++ c/cert/test/rules/MSC40-C/test.c
@@ -4,28 +4,28 @@ const int g3 = 1; // defaults to internal linkage
 
 extern inline void test1() {
   static int i = 0; // NON_COMPLIANT
-  g1++; // NON_COMPLIANT
-  g2++; // COMPLIANT
-  g3; // NON_COMPLIANT
+  g1++;             // NON_COMPLIANT
+  g2++;             // COMPLIANT
+  g3;               // NON_COMPLIANT
 }
 
 extern void test2() {
   static int i = 0; // COMPLIANT
-  g1++; // COMPLIANT
-  g2++; // COMPLIANT
-  g3; // COMPLIANT
+  g1++;             // COMPLIANT
+  g2++;             // COMPLIANT
+  g3;               // COMPLIANT
 }
 
 void test3() {
   static int i = 0; // COMPLIANT
-  g1++; // COMPLIANT
-  g2++; // COMPLIANT
-  g3; // COMPLIANT
+  g1++;             // COMPLIANT
+  g2++;             // COMPLIANT
+  g3;               // COMPLIANT
 }
 
 inline void test4() {
   static int i = 0; // NON_COMPLIANT
-  g1++; // NON_COMPLIANT
-  g2++; // COMPLIANT
-  g3; // NON_COMPLIANT
+  g1++;             // NON_COMPLIANT
+  g2++;             // COMPLIANT
+  g3;               // NON_COMPLIANT
 }
