00fa7604b5b3af21e3997456cfd7f1b259ac75c3
Remco Vermeulen <rvermeulen@users.noreply.github.com>
2024-01-30 15:36:40 -0800

    Change definition of out parameter and add exclusions
    An out parameter is a non-const reference or pointer that is modified.
    We removed assignment operators and crement operators as exclusions to
    the modified property, because they should still be used to qualify an
    out parameter.
    
    Additionally we exclude user-defined non-member assignment operator and
    stream related operator parameters that are required to be passed by
    non-const reference.

--- cpp/autosar/test/rules/A8-4-8/test.cpp
+++ cpp/autosar/test/rules/A8-4-8/test.cpp
@@ -5,7 +5,7 @@ void f(int &i) { // COMPLIANT
 void f1(std::string &str) { // NON_COMPLIANT
   str = "replacement";
 }
-void f2(std::string &str) { // COMPLIANT
+void f2(std::string &str) { // NON_COMPLIANT
   str += "suffix";
 }
 
@@ -36,4 +36,39 @@ void f7(A &a) { // NON_COMPLIANT
 
 void f8(int i) { // COMPLIANT
   i += 1;
+}
+
+constexpr A &operator|=(
+    A &lhs,
+    const A &rhs) noexcept { // COMPLIANT - non-member user defined assignment
+                             // operators are considered an exception.
+  return lhs;
+}
+
+enum class byte : unsigned char {};
+constexpr byte &operator|(const byte &lhs, const byte &rhs) noexcept {
+  return lhs | rhs;
+}
+constexpr byte &operator|=(
+    byte &lhs,
+    const byte rhs) noexcept { // COMPLIANT - non-member user defined assignment
+                               // operators are considered an exception.
+  lhs = (lhs | rhs);
+  return lhs;
+}
+
+#include <iostream>
+std::ostream &operator<<(std::ostream &os,
+                         const byte &obj) { // COMPLIANT - insertion operators
+                                            // are considered an exception.
+  std::ostream other;
+  os = other; // simulate modification
+  return os;
+}
+
+std::istream &operator>>(std::istream &is,
+                         byte &obj) { // COMPLIANT - extraction operators are
+                                      // considered an exception.
+  obj = static_cast<byte>('a');       // simulate modification
+  return is;
 }
