8c28f1e35ca565013883be1717f07183768537b6
rakesh.pothengil <rakesh.pothengil@woven-planet.global>
2024-07-23 11:24:01 +0900

    Added tests and generalized fix

---
+++ cpp/autosar/test/rules/M0-1-10.1/test.cpp
@@ -0,0 +1,34 @@
+#include <cstdint>
+
+namespace mains {
+static std::int32_t var;
+
+// @brief namespace_func
+static void
+namespace_func(void) noexcept { // COMPLIANT: Called from "main" below.
+  mains::var = -1;
+  return;
+}
+} // namespace mains
+
+std::int32_t func2() // COMPLIANT: Called from func1
+{
+  return mains::var + 20;
+}
+
+std::int32_t func1() { // COMPLIANT: Called from main
+  return mains::var + func2(); // func2 called here.
+}
+
+// @brief main
+// @return exit code
+std::int32_t main(void) {
+  std::int32_t ret{0};
+  try {
+    ret = func1(); // func1 called here.
+    mains::var += ret;
+  } catch (...) {
+    mains::namespace_func(); // namespace_func called here.
+  }
+  return ret;
+}
