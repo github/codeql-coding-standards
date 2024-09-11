8729c437e87d8aa6661e6819c3baaf351ba416b0
Luke Cartey <lcartey@github.com>
2024-09-11 14:55:25 +0100

    Rule 11.9: Format test file

--- c/misra/test/rules/RULE-11-9/test.c
+++ c/misra/test/rules/RULE-11-9/test.c
@@ -20,15 +20,15 @@ void *f1(void *p1, int p2) {
   if (p2 == 0) {  // COMPLIANT
     return NULL;
   }
-  p2 ? p1 : 0;     // NON_COMPLIANT
-  p2 ? 0 : p1;     // NON_COMPLIANT
-  p2 ? (void*) 0 : p1;     // COMPLIANT
-  p2 ? p1 : (void*) 0;     // COMPLIANT
-  p2 ? p2 : 0;     // COMPLIANT - p2 is not a pointer type
-  p2 ? 0 : p2;     // COMPLIANT - p2 is not a pointer type
+  p2 ? p1 : 0;         // NON_COMPLIANT
+  p2 ? 0 : p1;         // NON_COMPLIANT
+  p2 ? (void *)0 : p1; // COMPLIANT
+  p2 ? p1 : (void *)0; // COMPLIANT
+  p2 ? p2 : 0;         // COMPLIANT - p2 is not a pointer type
+  p2 ? 0 : p2;         // COMPLIANT - p2 is not a pointer type
   int x;
   int *y;
-  p2 ? (p1 = 0) : p1;     // NON_COMPLIANT - p1 is a pointer type
-  p2 ? (p2 = 0) : p1;     // COMPLIANT - p2 is not a pointer type
-  return 0;                             // COMPLIANT
+  p2 ? (p1 = 0) : p1; // NON_COMPLIANT - p1 is a pointer type
+  p2 ? (p2 = 0) : p1; // COMPLIANT - p2 is not a pointer type
+  return 0;           // COMPLIANT
 }
