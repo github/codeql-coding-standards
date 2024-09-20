9c0e6baa1ef4a6d890662bf7c18dcdb523fac63a
Mike Fairhurst <michaelrfairhurst@github.com>
2024-09-20 11:28:02 -0700

    clang format

--- c/misra/test/rules/RULE-6-3/test.c
+++ c/misra/test/rules/RULE-6-3/test.c
@@ -1,21 +1,21 @@
 union U1 {
-  int x; // COMPLIANT
+  int x;  // COMPLIANT
   char y; // COMPLIANT
 };
 
 union U2 {
-  int x: 2; // NON-COMPLIANT
-  char y; // COMPLIANT
+  int x : 2; // NON-COMPLIANT
+  char y;    // COMPLIANT
 };
 
 union U3 {
-    struct str {
-        int x: 4; // COMPLIANT
-        int y: 2; // COMPLIANT
-    };
+  struct str {
+    int x : 4; // COMPLIANT
+    int y : 2; // COMPLIANT
+  };
 };
 
 union U4 {
   char x;
-  int :0; // NON-COMPLIANT
+  int : 0; // NON-COMPLIANT
 };
