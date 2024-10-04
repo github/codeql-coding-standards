38a467ddd5c08caa083cbf66a220b6190cbda6d6
Mike Fairhurst <michaelrfairhurst@github.com>
2024-10-04 12:52:25 -0700

    Add cross-linking comments to tests referencing other rules.

--- c/misra/test/rules/RULE-8-8/test.c
+++ c/misra/test/rules/RULE-8-8/test.c
@@ -1,3 +1,8 @@
+// Note: A subset of these cases are also tested in c/misra/test/rules/RULE-1-5
+// via a MissingStaticSpecifierObjectRedeclarationC.qlref and .expected file in
+// that directory. Changes to these tests may require updating the test code or
+// expectations in that directory as well.
+
 static int g = 0;
 extern int g; // NON_COMPLIANT
 
