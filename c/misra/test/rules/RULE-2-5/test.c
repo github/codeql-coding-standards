dd07767d6a57731be2e43c29984183dff0c0aead
Luke Cartey <lcartey@github.com>
2024-10-22 22:58:36 +0100

    Rule 2.5: Consider macros accessed before definition

--- c/misra/test/rules/RULE-2-5/test.c
+++ c/misra/test/rules/RULE-2-5/test.c
@@ -14,3 +14,34 @@ void test() {
   MACRO2;
   HEADER_MACRO2;
 }
+
+#define CHECKED_MACRO_1 // COMPLIANT - used in branch
+#define CHECKED_MACRO_2 // COMPLIANT - used in branch
+#define CHECKED_MACRO_3 // COMPLIANT - used in branch
+
+#ifdef CHECKED_MACRO_1
+#endif
+
+#ifndef CHECKED_MACRO_2
+#endif
+
+#if defined(CHECKED_MACRO_3)
+#endif
+
+// In the case above, the extractor will identify macro accesses with each use
+// of the macro. In the case above, the extractor does not tie them together,
+// but the standard considers this acceptable usage. Notably, this type of
+// pattern occurs for header guards.
+
+#ifdef CHECKED_MACRO_BEFORE_1
+#endif
+
+#ifndef CHECKED_MACRO_BEFORE_2
+#endif
+
+#if defined(CHECKED_MACRO_BEFORE_3)
+#endif
+
+#define CHECKED_MACRO_BEFORE_1 // COMPLIANT - used in branch
+#define CHECKED_MACRO_BEFORE_2 // COMPLIANT - used in branch
+#define CHECKED_MACRO_BEFORE_3 // COMPLIANT - used in branch
