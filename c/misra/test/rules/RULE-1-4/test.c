137329219c0e1c200f94f2bfe19e8c851636d24a
Mike Fairhurst <michaelrfairhurst@github.com>
2025-02-09 00:38:27 -0800

    s/NON-COMPLIANT/NON_COMPLIANT in rule-1-4 test.c

--- c/misra/test/rules/RULE-1-4/test.c
+++ c/misra/test/rules/RULE-1-4/test.c
@@ -1,15 +1,15 @@
 #include <stdalign.h>    //COMPLIANT
-#include <stdatomic.h>   //NON-COMPLIANT
+#include <stdatomic.h>   //NON_COMPLIANT
 #include <stdnoreturn.h> //COMPLIANT
-#include <threads.h>     //NON-COMPLIANT
+#include <threads.h>     //NON_COMPLIANT
 
-#define MACRO(x) _Generic((x), int : 0, long : 1) // NON-COMPLIANT
-#define __STDC_WANT_LIB_EXT1__ 1                  // NON-COMPLIANT
+#define MACRO(x) _Generic((x), int : 0, long : 1) // NON_COMPLIANT
+#define __STDC_WANT_LIB_EXT1__ 1                  // NON_COMPLIANT
 
 _Noreturn void f0(); // COMPLIANT
 
 typedef int new_type;                     // COMPLIANT
-typedef _Atomic new_type atomic_new_type; // NON-COMPLIANT
+typedef _Atomic new_type atomic_new_type; // NON_COMPLIANT
 
 void f(int p) {
   int i0 = _Generic(p, int : 0, long : 1); // COMPLIANT
@@ -21,6 +21,6 @@ void f(int p) {
   int a = _Alignof(int); // COMPLIANT
   int a1 = alignof(int); // COMPLIANT
 
-  static thread_local int i3;  // NON-COMPLIANT
-  static _Thread_local int i4; // NON-COMPLIANT
+  static thread_local int i3;  // NON_COMPLIANT
+  static _Thread_local int i4; // NON_COMPLIANT
 }
