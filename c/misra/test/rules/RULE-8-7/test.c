70c776ae2148b7b4a66134ba9c9c47846d8345d0
Luke Cartey <lcartey@github.com>
2025-03-09 11:24:54 +0000

    RULE-8-7: Restructure test
    Move single translation unit items into the translation unit, not
    the shared header.

--- c/misra/test/rules/RULE-8-7/test.c
+++ c/misra/test/rules/RULE-8-7/test.c
@@ -1,4 +1,7 @@
 #include "test.h"
+int i2;           // NON_COMPLIANT - accessed one translation unit
+static void f3(); // COMPLIANT - internal linkage
+extern void f3(); // COMPLIANT - internal linkage
 void f() {
   i = 0;
   i1 = 0;
