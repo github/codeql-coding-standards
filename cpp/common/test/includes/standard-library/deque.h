b44c675184064f99dbc783f754eb1f42e65a4ea4
Mike Fairhurst <michaelrfairhurst@github.com>
2026-02-18 17:47:15 -0800

    Add default ctor to deque which was breaking tests.

--- cpp/common/test/includes/standard-library/deque.h
+++ cpp/common/test/includes/standard-library/deque.h
@@ -13,6 +13,7 @@ public:
   typedef value_type &reference;
   typedef const value_type &const_reference;
 
+  deque() = default;
   deque(std::initializer_list<T>, const Allocator & = Allocator());
 
   typedef __iterator<T> iterator;
