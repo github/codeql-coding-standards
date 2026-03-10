5d2f62f1fb06d92227c226a7b2a6ce05db315f39
Jeongsoo Lee <jeongsoolee09@github.com>
2026-03-10 10:05:38 -0700

    Update pointer_only.cpp according to test.cpp

--- cpp/misra/test/rules/RULE-8-7-1/pointer_only.cpp
+++ cpp/misra/test/rules/RULE-8-7-1/pointer_only.cpp
@@ -129,21 +129,23 @@ void realloc_single_dimensional_array_access(int *array) { // [3, 6]
 void stack_allocated_multi_dimensional_array_access(int array[2][3]) {
   int valid11 = array[0][0];  // COMPLIANT: pointer is within boundary
   int valid12 = array[0][1];  // COMPLIANT: pointer is within boundary
-  int valid13 = array[0][2];  // COMPLIANT: pointer points one beyond the last
+  int valid13 = array[0][2];  // COMPLIANT: pointer is within boundary
+  int valid13 = array[0][3];  // COMPLIANT: pointer points one beyond the last
                               // element, but non-compliant to Rule 4.1.3
-  int invalid1 = array[0][3]; // NON_COMPLIANT: pointer points more than one
+  int invalid1 = array[0][4]; // NON_COMPLIANT: pointer points more than one
                               // beyond the last element
 
-  int valid21 = array[1][0]; // COMPLIANT: pointer is within boundary
-  int valid22 = array[1][1]; // COMPLIANT: pointer is within boundary
-  int valid23 = array[1][2]; // COMPLIANT: pointer points one beyond the last
+  int valid21 = array[1][0];  // COMPLIANT: pointer is within boundary
+  int valid22 = array[1][1];  // COMPLIANT: pointer is within boundary
+  int valid23 = array[1][2];  // COMPLIANT: pointer is within boundary
+  int valid13 = array[1][3];  // COMPLIANT: pointer points one beyond the last
+                              // element, but non-compliant to Rule 4.1.3
+  int invalid2 = array[1][4]; // NON_COMPLIANT: pointer points more than one
+                              // beyond the last element
+
+  int valid31 = array[2][0]; // COMPLIANT: pointer points one beyond the last
                              // element, but non-compliant to Rule 4.1.3
 
-  int invalid2 = array[1][3]; // NON_COMPLIANT: pointer points more than one
-                              // beyond the last element
-
-  int valid31 = array[2][0];  // COMPLIANT: pointer points one beyond the last
-                              // element, but non-compliant to Rule 4.1.3
   int invalid3 = array[3][0]; // NON_COMPLIANT: pointer points more than one
                               // beyond the last element
 }
@@ -159,17 +161,16 @@ void stack_allocated_multi_dimensional_pointer_arithmetic(int array[2][3]) {
   int valid122 =
       *(*array +
         1); // COMPLIANT: pointer is within boundary (equivalent to the above)
-  int valid131 =
-      *(*(array + 0) + 2); // COMPLIANT: pointer points one beyond the last
-                           // element, but non-compliant to Rule 4.1.3
-  int valid132 = *(
-      *array +
-      2); // COMPLIANT: pointer points one beyond the last
-          // element, but non-compliant to Rule 4.1.3 (equivalent to the above)
-  int invalid11 = *(*(array + 0) + 3); // NON_COMPLIANT: pointer points more
+  int valid131 = *(*(array + 0) + 2); // COMPLIANT: pointer is within boundary
+  int valid132 = *(*array + 2);       // COMPLIANT: pointer is within boundary
+  int valid141 = *(*(array + 0) + 3); // COMPLIANT: pointer points to one beyond
+                                      // the last element
+  int valid142 = *(*array + 3); // COMPLIANT: pointer points to one beyond the
+                                // last element (equivalent to the above)
+  int invalid11 = *(*(array + 0) + 4); // NON_COMPLIANT: pointer points more
                                        // than one beyond the last element
   int invalid12 =
-      *(*array + 3); // NON_COMPLIANT: pointer points more than
+      *(*array + 4); // NON_COMPLIANT: pointer points more than
                      // one beyond the last element (equivalent to the above)
 
   int valid211 = *(*(array + 1) + 0); // COMPLIANT: pointer is within boundary
@@ -177,19 +178,18 @@ void stack_allocated_multi_dimensional_pointer_arithmetic(int array[2][3]) {
       *(array +
         1)); // COMPLIANT: pointer is within boundary (equivalent to the above)
   int valid22 = *(*(array + 1) + 1); // COMPLIANT: pointer is within boundary
-  int valid23 =
-      *(*(array + 1) + 2); // COMPLIANT: pointer points one beyond the last
-                           // element, but non-compliant to Rule 4.1.3
-  int invalid2 = *(*(array + 1) + 3); // NON_COMPLIANT: pointer points more than
+  int valid23 = *(*(array + 1) + 2); // COMPLIANT: pointer points one beyond the
+                                     // last element
+  int valid24 =
+      *(*(array + 1) +
+        3); // COMPLIANT: pointer points to one beyond the last element
+  int invalid2 = *(*(array + 1) + 4); // NON_COMPLIANT: pointer points more than
                                       // one beyond the last element
 
-  int valid311 =
-      *(*(array + 2) + 0); // COMPLIANT: pointer points one beyond the last
-                           // element, but non-compliant to Rule 4.1.3
-  int valid312 = *(*(
-      array +
-      2)); // COMPLIANT: pointer points one beyond the last
-           // element, but non-compliant to Rule 4.1.3 (equivalent to the above)
+  int valid311 = *(*(array + 2) + 0); // COMPLIANT: pointer points one beyond
+                                      // the last element
+  int valid312 = *(*(array + 2)); // COMPLIANT: pointer points one beyond the
+                                  // last element (equivalent to the above)
   int invalid31 = *(*(array + 3) + 0); // NON_COMPLIANT: pointer points more
                                        // than one beyond the last element
   int invalid32 =
