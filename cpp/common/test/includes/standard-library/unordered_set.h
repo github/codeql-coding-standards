fa83b3d64937f74604048a0679f873bfb90baf0f
jeongsoolee09 <jeongsoolee09@github.com>
2026-04-20 17:50:28 -0400

    Add more template parameters to the stubs to match the spec

--- cpp/common/test/includes/standard-library/unordered_set.h
+++ cpp/common/test/includes/standard-library/unordered_set.h
@@ -2,25 +2,30 @@
 #define _GHLIBCPP_UNORDERED_SET
 
 #include <initializer_list>
+#include <vector>
 
 namespace std {
 
-template<typename T>
+template <typename T, typename Hash = std::hash<T>,
+          typename Equal = std::equal_to<T>,
+          typename Allocator = std::allocator<T>>
 class unordered_set {
 public:
-    unordered_set();
-    unordered_set(const unordered_set&);
-    unordered_set(unordered_set&&);
-    unordered_set(std::initializer_list<T>);
+  unordered_set();
+  unordered_set(const unordered_set &);
+  unordered_set(unordered_set &&);
+  unordered_set(std::initializer_list<T>);
 };
 
-template<typename T>
+template <typename T, typename Hash = std::hash<T>,
+          typename Equal = std::equal_to<T>,
+          typename Allocator = std::allocator<T>>
 class unordered_multiset {
 public:
-    unordered_multiset();
-    unordered_multiset(const unordered_multiset&);
-    unordered_multiset(unordered_multiset&&);
-    unordered_multiset(std::initializer_list<T>);
+  unordered_multiset();
+  unordered_multiset(const unordered_multiset &);
+  unordered_multiset(unordered_multiset &&);
+  unordered_multiset(std::initializer_list<T>);
 };
 
 } // namespace std
