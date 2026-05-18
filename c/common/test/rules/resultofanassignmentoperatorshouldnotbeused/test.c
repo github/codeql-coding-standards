36092b7fde83319d4c62a51cd844e42afbf21cc7
Mauro Baluda <mbaluda@github.com>
2026-05-18 22:12:29 +0200

    Fix FP for MISRA-C RULE-13-4

--- c/common/test/rules/resultofanassignmentoperatorshouldnotbeused/test.c
+++ c/common/test/rules/resultofanassignmentoperatorshouldnotbeused/test.c
@@ -13,4 +13,15 @@ void test() {
   l1 = l3[l2 = 0]; // NON_COMPLIANT
 
   l1 = l2 = 0; // NON_COMPLIANT
+
+  l3[l1 += l2] = l3[l1]; // NON_COMPLIANT
+
+  for (l1 = 0; l1 < 10; l1 += l2) // COMPLIANT
+  {
+  }
+
+  while (l1 < 10) // COMPLIANT
+  {
+    l1 += l2; // COMPLIANT
+  }
 }
