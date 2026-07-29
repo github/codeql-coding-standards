803dbeb49eae475403226a8083f313d45f655f8e
Jeroen Ketema <jketema@github.com>
2026-07-29 16:02:43 +0200

    Disable tests that no longer work with the extractor

--- cpp/misra/test/rules/RULE-7-0-6/test.cpp
+++ cpp/misra/test/rules/RULE-7-0-6/test.cpp
@@ -371,7 +371,7 @@ std::int32_t f12(std::int8_t l1) {
   std::int16_t l2 = l1;     // COMPLIANT
   std::int16_t l3{l1};      // COMPLIANT
   std::int16_t l4(l1);      // COMPLIANT
-  std::int16_t l5{l1 + l1}; // NON_COMPLIANT
+  // std::int16_t l5{l1 + l1}; // NON_COMPLIANT - Not accepted by clang or by CodeQL when mimicking clang
   return l1;                // COMPLIANT
 }
 
